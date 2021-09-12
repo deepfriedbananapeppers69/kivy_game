f110427f36e8bcae5a857f3704cf78c760c93bb3
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-09-11 21:15:28 -0700

    kjhkhabds

--- test.py
+++ test.py
@@ -1,46 +1,24 @@
 import random
-from ctypes import string_at
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class my_grid(Widget):
-    pass
-    '''
-    def __init__(self, **kwargs):
-        super(my_grid, self).__init__(**kwargs)
-        self.cols = 1 
-        self.inside = GridLayout()
-        self.inside.cols = 2
-        self.string1 = "vbhjvba"
-        self.label1 = Label()
-        self.inside.add_widget(self.label1)
-        self.name = TextInput()
-        self.inside.add_widget(self.name)
-              
-        
-        self.add_widget(self.inside)
-        
-        self.sumbit = Button(text="ljknakjfj", font_size=40) 
-        self.sumbit.bind(on_press=self.pressed)
-        self.add_widget(self.sumbit)
-        
-    def pressed(self, instance):
-        self.number = random.randint(0,5)       
-        text = self.name.text
-        print(text)
-        self.name.text = ""
-        self.label1.text = str(self.number)
+    nocal = ObjectProperty(None)
+    socal = ObjectProperty(None)
+    choice = ObjectProperty(None)
+    score = ObjectProperty(None)
     
-    def changelabel(self, instance):
-        self.string1 = "hello"
-        self.label1 = self.label1
-     '''      
-        
+    def btn(self):
+        self.numberscore = 0 
+        self.number = random.randint(0, 9)
+        self.choice.text = str(self.number)
+        self.score.text =   
         
 class MyApp(App):
     def build(self):
