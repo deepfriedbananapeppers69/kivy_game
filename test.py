19d69a6d767fd7cae817b699742f47ecc130689d
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-09-19 14:59:47 -0700

    qivehb

--- test.py
+++ test.py
@@ -6,23 +6,167 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from openpyxl import workbook, load_workbook
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-
-class my_grid(Widget):
+class my_grid(Screen, Widget):
     nocal = ObjectProperty(None)
     socal = ObjectProperty(None)
-    choice = ObjectProperty(None)
-    score = ObjectProperty(None)
+    desert = ObjectProperty(None)
+    vancouver = ObjectProperty(None)
+    seattle = ObjectProperty(None)
+    other = ObjectProperty(None)
+    choicelabel = ObjectProperty(None)
+    scorelable = ObjectProperty(None)
+    message = ObjectProperty(None)
+    wb = load_workbook('MASTEREXCEL.xlsx')
+    wsPM = wb["PMCODES"]
+    score = 0
+
+
     
+    
+    
+    SoCal = []
+    for col in wsPM['A']:
+        SoCal.append(col.value)
+    SoCal = [i for i in SoCal if i != None]
+
+    NoCal = []
+    for col in wsPM['B']:
+        NoCal.append(col.value)
+    NoCal = [i for i in NoCal if i != None]
+
+    Desert = []
+    for col in wsPM['C']:
+        Desert.append(col.value)
+    Desert = [i for i in Desert if i != None]
+        
+    Vancouver = []
+    for col in wsPM['D']:
+        Vancouver.append(col.value)
+    Vancouver = [i for i in Vancouver if i != None]
+
+    Seattle = []
+    for col in wsPM['E']:
+        Seattle.append(col.value)
+    Seattle = [i for i in Seattle if i != None]
+
+    Other = []
+    for col in wsPM['F']:
+        Other.append(col.value)
+    Other = [i for i in Other if i != None]
+
+    fullList = list(SoCal + NoCal + Desert + Vancouver + Seattle + Other)
+    choicevar = random.choice(fullList)
+    
+        
     def btn(self):
         self.numberscore = 0 
-        self.number = random.randint(0, 9)
-        self.choice.text = str(self.number)
-        self.score.text =   
-        
+        self.choicevar = random.choice(self.fullList)
+        self.choicelabel.text = str(self.choicevar)
+    
+    def NOCALCHECK (self,):
+        answer = self.choicevar
+        if answer in self.NoCal:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    def SOCALCHECK (self,):
+        answer = self.choicevar
+        self.score = 0
+        if answer in self.SoCal:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    def DESERTCHECK (self,):
+        answer = self.choicevar
+        if answer in self.Desert:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    def VANCOUVERCHECK (self,):
+        answer = self.choicevar
+        if answer in self.Vancouver:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    def SEATTLECHECK (self,):
+        answer = self.choicevar
+        if answer in self.Seattle:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    def OTHERCHECK (self,):
+        answer = self.choicevar
+        if answer in self.Other:
+            self.message.text = "correct"
+            self.choicevar = random.choice(self.fullList)
+            self.choicelabel.text = str(self.choicevar)
+            self.score += 1
+            self.scorelabel.text = str(self.score)
+        else:
+            self.message.text = "wrong"
+            self.score -=1
+            if self.score < 0:
+                self.score = 0
+            self.scorelabel.text = str(self.score)
+    
+    
+
+
+
+
+
 class MyApp(App):
+    
     def build(self):
-        return my_grid()
+        
+        sm = ScreenManager()
+        sm.add_widget(my_grid(name='game'))
+        sm.add_widget()
+        return sm
             
 
 
